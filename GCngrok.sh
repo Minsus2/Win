7b779582e026d51ff0d27c82695ee73db92da5c0
Minsus2 <130376258+Minsus2@users.noreply.github.com>
2023-04-10 23:18:11 +0700

    Create GCngrok.sh

---
+++ GCngrok.sh
@@ -0,0 +1,4 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+./ngrok authtoken 2OEpgIWKlFMdGUJjVf3tyqDnnXj_7rDLzj9p3Jnvwj9Vj2D2P
